b1ae297ed41283b72002b1170d9a454bea4d3a89
antoineg <antoine.braillon@lecole-ldlc.com>
2025-06-16 17:56:10 -1200

    config

--- config.php
+++ config.php
@@ -3,9 +3,9 @@
 session_start();
 
 // Paramètres de connexion à la base de données
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'plateforme_cours');
-define('DB_USER', 'root'); // Remplacez par votre nom d'utilisateur MySQL
+define('DB_HOST', '10.96.16.82');
+define('DB_NAME', 'ecole');
+define('DB_USER', 'colin'); // Remplacez par votre nom d'utilisateur MySQL
 define('DB_PASS', ''); // Remplacez par votre mot de passe MySQL
 
 // Fonction de connexion à la base de données
