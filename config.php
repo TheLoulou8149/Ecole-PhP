10ed8ed0951e964930a203069f073ba8a9e19913
Morgane <fromenteaumorgane@gmail.com>
2025-06-17 17:43:44 +0200

    log

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define('DB_HOST', '10.96.16.82');
 define('DB_NAME', 'ecole');
 define('DB_USER', 'colin');
 define('DB_PASS', '');
-
+}
 try {
     $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4';
     $pdo = new PDO($dsn, DB_USER, DB_PASS);
@@ -13,4 +13,3 @@ try {
     die("Erreur de connexion à la base de données : " . $e->getMessage());
 }
 ?>
-} 
