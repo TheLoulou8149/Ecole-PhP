d0aab888e2dec89e66356a82a78e65bb911a9b75
antoineg <antoine.braillon@lecole-ldlc.com>
2025-06-17 14:22:50 -1200

    profil.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ session_start();
 define('DB_HOST', '10.96.16.82');
 define('DB_NAME', 'ecole');
 define('DB_USER', 'colin'); // Remplacez par votre nom d'utilisateur MySQL
-define('DB_PASS', ''); // Remplacez par votre mot de passe MySQL
+define('DB_PASS', ''); // Remplacez par v.otre mot de passe MySQL
 
 try {
     $pdo = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8", DB_USER, DB_PASS);
