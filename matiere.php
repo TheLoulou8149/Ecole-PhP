0a2c10f895aa3e7c98d72c377e8f7a4910a84a0f
pablo <pablo@mail.com>
2025-06-18 13:45:05 +0200

    matiere avec header2

--- matiere.php
+++ matiere.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mes Cours - École</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <style>
+     <?php include 'header.php'; ?>
+   <style>
         :root {
             --primary: #6c5ce7;
             --secondary: #a29bfe;
